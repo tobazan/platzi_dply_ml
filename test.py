34d831a68827670fee87fb74aced47796eacd874
tobazan <bazanme90@gmail.com>
2022-07-28 07:52:22 -0300

    dockerized api

--- test.py
+++ test.py
@@ -21,15 +21,15 @@ def test_null_prediction():
 
 def test_random_prediction():
     response = client.post('/v1/prediction', json = {
-                                                    'opening_gross': 8330681,
-                                                    'screens': 2271,
-                                                    'production_budget': 13000000,
-                                                    'title_year': 1999,
-                                                    'aspect_ratio': 1.85,
-                                                    'duration': 97,
-                                                    'cast_total_facebook_likes': 37907,
-                                                    'budget': 16000000,
-                                                    'imdb_score': 7.2
+                                                    "opening_gross": 8330681,
+                                                    "screens": 2271,
+                                                    "production_budget": 13000000,
+                                                    "title_year": 1999,
+                                                    "aspect_ratio": 1.85,
+                                                    "duration": 97,
+                                                    "cast_total_facebook_likes": 37907,
+                                                    "budget": 16000000,
+                                                    "imdb_score": 7.2
                                                 })
     assert response.status_code == 200
     assert response.json()['worldwide_gross'] != 0 
